0aa1e923f30196760ee9b5aa195dd029c953d622
edaway <ethereos@gmail.com>
2024-11-02 15:49:45 -0400

    added helmet to monkey

--- Assets/Scripts/CharControl/grappleScript.cs
+++ Assets/Scripts/CharControl/grappleScript.cs
@@ -24,6 +24,7 @@ public class Grapple : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Cursor.transform.position);
         if (Input.GetKeyDown(hand) && Physics.Raycast(ray, out RaycastHit cursorHit, 100))
         {
+            playerBody.isKinematic = false;
             Reticle.transform.position = cursorHit.point;
         }
 
