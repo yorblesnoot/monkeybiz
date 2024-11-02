f52bb37823173bd02c7ff343ff8c6d92aa8fb27c
Chris Schmidt <cmschmidt12345@gmail.com>
2024-11-02 14:55:06 -0400

    working post combination chris pc

--- Assets/Scripts/Level/SectionTrigger.cs
+++ Assets/Scripts/Level/SectionTrigger.cs
@@ -1,15 +1,43 @@
+using System;
 using UnityEngine;
 
 public class SectionTrigger : MonoBehaviour
 {
     public GameObject levelSection;
+    public GameObject levelSection2;
+    public GameObject levelSection3;
     public int levelNumber = 0;
+    public int levelSelector = 1;
+    public string levelGenerated = "Nope";
 
 	private void OnTriggerEnter(Collider other)
 	{
+        levelSelector = UnityEngine.Random.Range(1, 4);
+        //print($"levelSelector is: {levelSelector}");
+
         if (other.gameObject.CompareTag("New Section Trigger"))
         {
-            Instantiate(levelSection, new Vector3(-15, 0, (levelNumber * 30 + 15)), Quaternion.identity);
+            if (levelSelector == 1)
+            {
+                //levelGenerated = "LevelSelection";
+				Instantiate(levelSection, new Vector3(-15, 0, (levelNumber * 30 + 15)), Quaternion.identity);
+			}
+            else if (levelSelector == 2)
+            {
+				//levelGenerated = "LevelSelection2";
+				Instantiate(levelSection2, new Vector3(-15, 0, (levelNumber * 30 + 15)), Quaternion.identity);
+			}
+            else if (levelSelector == 3)
+            {
+				//levelGenerated = "LevelSelection3";
+				Instantiate(levelSection3, new Vector3(-15, 0, (levelNumber * 30 + 15)), Quaternion.identity);
+			}
+            else
+            {
+                Console.WriteLine(levelGenerated, " we may have a problem");
+            }
+
+			print($"level Generated is: {levelGenerated}");
             levelNumber++;
         }
 	}
