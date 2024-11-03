efe9d9f6c9c5634a722a0958c4f6a8e514ecea66
echoatday <echoatday@gmail.com>
2024-11-03 15:10:24 -0500

    rotate menu background

---
+++ Assets/Scripts/Level/rotateZaxis1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateZaxis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    [SerializeField] int rotationsPerMinute = 10;
+    [SerializeField] int spinDirection = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 6.0f * spinDirection * rotationsPerMinute * Time.deltaTime);
+    }
+}
